1942dc83df6b972102b40bef8dc8d82d073980c9
SooLee <duplexa@gmail.com>
2016-11-04 16:52:55 -0400

    format change for downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-## SOFTWARE: samtools-1.2
+## SOFTWARE: samtools
+## VERSION: 1.2
 wget https://github.com/samtools/samtools/releases/download/1.2/samtools-1.2.tar.bz2
 tar -xjf samtools-1.2.tar.bz2
 cd samtools-1.2
